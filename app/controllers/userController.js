1ac30cfc5da217cc88bd13c1aca9e0905b2e63ec
zak530 <elhaiki.zakaria.studente@itispaleocapa.it>
2026-05-06 16:44:29 +0000

    aggiunta del prezzo e della quantita in magazzino

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,19 +1,26 @@
-// app/controllers/userController.js
 const User = require('../models/userModel');
+
 exports.home = (req, res) => {
   res.render('index');
 };
+
 exports.listUsers = (req, res) => {
   const users = User.findAll();
   res.render('users', { users });
 };
+
 exports.createUser = (req, res) => {
-  const { name, email, linkImg } = req.body;
-  User.create(name, email, linkImg);
+  const { name, email, linkImg, prezzo, quantitaMagazzino } = req.body;
+
+  User.create(name, email, linkImg, prezzo, quantitaMagazzino);
+
   res.redirect('/users');
 };
+
 exports.deleteUser = (req, res) => {
   const { id } = req.params;
+
   User.delete(id);
+
   res.redirect('/users');
 };
--- app/models/userModel.js
+++ app/models/userModel.js
@@ -1,15 +1,29 @@
-// app/models/userModel.js
 let users = [];
+
 const User = {
   findAll: () => users,
-  create: (name, email, linkImg) => {
-    const newUser = { id: Date.now(), name, email, linkImg };
+
+  create: (name, email, linkImg, prezzo, quantitaMagazzino) => {
+    const newUser = {
+      id: Date.now(),
+      name: name,
+      email: email,
+      linkImg: linkImg,
+      prezzo: prezzo,
+      quantitaMagazzino: quantitaMagazzino
+    };
+
     users.push(newUser);
     return newUser;
   },
-  findById: (id) => users.find(u => u.id === parseInt(id)),
+
+  findById: (id) => {
+    return users.find(u => u.id === parseInt(id));
+  },
+
   delete: (id) => {
     users = users.filter(u => u.id !== parseInt(id));
   }
 };
+
 module.exports = User;
